958ef4c9b0ddbffa4437773e70034473c83b6839
Akash Pal <73605611+akashcodepal@users.noreply.github.com>
2021-07-12 09:32:32 +0530

    Create Number of 1 Bits.js

---
+++ Leet Code Week 03/Number of 1 Bits.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let count = 0
+    while(n) {
+        if (n%2 == 1) 
+            count++
+        n = parseInt(n/2)
+    }
+    return count
+};
